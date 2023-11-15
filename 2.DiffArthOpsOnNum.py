a37601f5b794c5ce55929c71ae1ad97887fdc620
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-15 15:24:49 +0530

    Add files via upload

---
+++ 2.DiffArthOpsOnNum.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a value:"))
+b = int(input("Enter b value:"))
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+floor_div = a // b
+exponent = a ** b
+modulus = a % b
+print('Addition of', a, 'and', b, 'is', add)
+print('Subtraction of', a, 'and', b, 'is', sub)
+print('Multiplication of', a, 'and', b, 'is', mul)
+print('Division of', a, 'and', b, 'is', div)
+print('Floor Division of', a, 'and', b, 'is', floor_div)
+print('Exponent of', a, 'and', b, 'is', exponent)
+print('Modulus of', a, 'and', b, 'is', modulus)
