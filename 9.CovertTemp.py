67012c35b0329005d005c5f6a996640d85b5c3b2
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-15 16:55:29 +0530

    Add files via upload

---
+++ 9.CovertTemp.py
@@ -0,0 +1,14 @@
+# Write a Python program to convert tempatures to and from Celsius, Fahrenheit. [Formula: c/5=f-32/9]
+print("1. To Covert temperature from Celsius to Fahrenheit")
+print("2. To Convert temperature from Fahrenheit to Celsius")
+opt = int(input("Choose any option:"))
+if opt == 1:
+    cel = float(input("Enter temperature in Celsius:"))
+    fahr = (cel*9/5)+32
+    print("Temperature in Fahrenheit=", fahr)
+elif opt == 2:
+    fahr = float(input("Enter temperature in Fahrenhite:"))
+    cel = (fahr-32)*5/9
+    print("Temperature in Celsius=", cel)
+else:
+    print("Invalid option!!!")
