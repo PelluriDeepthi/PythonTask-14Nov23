cf10798c07d0de7193ff23141d8b85e797704feb
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-15 16:06:53 +0530

    Add files via upload

---
+++ 5.CreateAppendRemoveList.py
@@ -0,0 +1,9 @@
+# Write a program to create, append and remove lists in python
+mylist = [2, 4, 6, 8, 10]
+print("Elements in list are:", mylist)
+
+mylist.append(12)
+print("List after appending:", mylist)
+
+mylist.remove(6)
+print("List after removing:", mylist)
