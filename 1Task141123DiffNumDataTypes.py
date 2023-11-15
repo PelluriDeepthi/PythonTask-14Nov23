6303aee5846381a07045a70a3e236a4d84b8d3df
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-15 12:51:56 +0530

    Add files via upload

---
+++ 1Task141123DiffNumDataTypes.py
@@ -0,0 +1,8 @@
+# Write A Program To Demonstrate Different Number Data Types in Python
+integernum = 15
+floatnum = 12.5
+complexnum = 3+7j
+
+print(integernum, 'is of type', type(integernum))
+print(floatnum, 'is of type', type(floatnum))
+print(complexnum, 'is of type', type(complexnum))
