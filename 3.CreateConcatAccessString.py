730915e82b8c51fcea9355073f63976fbab32e90
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-15 15:47:04 +0530

    Add files via upload

---
+++ 3.CreateConcatAccessString.py
@@ -0,0 +1,10 @@
+# Write a program to create, concatenate and print a string and accessing sub-string from a given string
+str1 = input("Enter a string: ")
+print("String is:", str1)
+
+str2 = input("Enter second string: ")
+string = str1+str2
+print("Concatenations of two strings are:", string)
+
+subStr = str[2:7]
+print("Sub String from given string is:", subStr)
