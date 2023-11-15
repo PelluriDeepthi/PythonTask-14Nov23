272b921e9fe12e25ebd3197ae21d198bc38cb211
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-15 15:29:28 +0530

    Update 2.DiffArthOpsOnNum.py

--- 2.DiffArthOpsOnNum.py
+++ 2.DiffArthOpsOnNum.py
@@ -1,16 +1,17 @@
-a = int(input("Enter a value:"))
-b = int(input("Enter b value:"))
+# Write a program to perform different Arithmetic Operations on numbers in Python
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
 add = a + b
 sub = a - b
 mul = a * b
 div = a / b
+modulus = a % b
 floor_div = a // b
 exponent = a ** b
-modulus = a % b
 print('Addition of', a, 'and', b, 'is', add)
 print('Subtraction of', a, 'and', b, 'is', sub)
 print('Multiplication of', a, 'and', b, 'is', mul)
 print('Division of', a, 'and', b, 'is', div)
+print('Modulus of', a, 'and', b, 'is', modulus)
 print('Floor Division of', a, 'and', b, 'is', floor_div)
 print('Exponent of', a, 'and', b, 'is', exponent)
-print('Modulus of', a, 'and', b, 'is', modulus)
