0e580cacc8cf93b37edd4cfad100a63cafa15e94
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-11-16 12:57:41 +0530

    Create 14.Fibonacci.py

---
+++ 14.Fibonacci.py
@@ -0,0 +1,4 @@
+#Write a python program to define a module to find Fibonacci Numbers and import the module to another program
+import FibonacciNum
+num = int(input("Enter a number to print Fibonacci Series:"))
+Fibonacci.fib(num)
